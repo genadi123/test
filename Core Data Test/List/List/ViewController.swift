6442236b0d3868f25c80a432cc1ecfb53d96c13b
genadi123 <genadi.betsuashvili@gmail.com>
2015-02-07 18:40:08 +0400

    Core Data  test
    1

---
+++ Core Data Test/List/List/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by robert on 2/7/15.
+//  Copyright (c) 2015 Genadi Betsuashvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    var names = [String]()
+    
+    @IBAction func addName(sender: AnyObject) {
+
+        var alert = UIAlertController(title: "New name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.names.append(textField.text)
+                self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        title = "The List"
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        cell.textLabel!.text = names[indexPath.row]
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
